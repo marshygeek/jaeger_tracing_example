7fd6aa1003dd7c73509b7aa5fd2f3e0ce7483551
Lester Marshy <lester0578@gmail.com>
2018-09-27 18:38:11 +0300

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,12 @@ class Logger:
     def __init__(self):
         self._engine = LoggerEngine()
 
-    def log(self, scenario, service_type, log_data):
+    def log(self):
+        # Data for testing
+        scenario = {'id': '123', 'type': 1}
+        service_type = 1
+        log_data = {'ev': 'testing'}
+        
         scenario_id = scenario['id']
         if not self._engine.is_exists(scenario_id):
             scenario_name = scenario_names[scenario['type']]
@@ -14,5 +19,7 @@ class Logger:
 
         self._engine.log(scenario_id, log_data)
 
-    def finish_scenario(self, scenario_id):
+    def finish_scenario(self):
+        # Data for testing
+        scenario_id = '123'
         self._engine.finish_scenario(scenario_id)
